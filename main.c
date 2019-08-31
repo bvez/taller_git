aabb72ab0a1c3617b1687fab3a19276e7a41b68e
bvez <bruno.espezua@pucp.pe>
2019-08-31 12:08:12 -0500

    se agrega funcion

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+void imprimir(){
+    printf("Esta impresion la hizo master");
+}
+
 int main(){
     printf("Hola mundo");
+    printf("Hola desde master");
     return 0;
 }
